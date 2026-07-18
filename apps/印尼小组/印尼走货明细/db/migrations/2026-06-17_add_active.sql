bddb061c53b8cb3bed4665d2ac276bd30deb5293
fxxaxxx <93503881+fxxaxxx@users.noreply.github.com>
2026-07-18 19:13:59 +0800

    Revert "feat: add Indonesia shipping portal (indo-shipping) (#280)" (#282)
    This reverts commit 0217b7bafd3d7c644bd259687edba861bb16cd34.
    
    Co-authored-by: hufan4308-blip <hufan4308@gmail.com>

--- apps/印尼小组/印尼走货明细/db/migrations/2026-06-17_add_active.sql
+++
@@ -1,4 +0,0 @@
-IF COL_LENGTH('dbo.products','active') IS NULL
-    ALTER TABLE dbo.products ADD active BIT NOT NULL CONSTRAINT DF_products_active DEFAULT 1;
-IF COL_LENGTH('dbo.customers','active') IS NULL
-    ALTER TABLE dbo.customers ADD active BIT NOT NULL CONSTRAINT DF_customers_active DEFAULT 1;
